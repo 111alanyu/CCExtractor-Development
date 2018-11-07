444b76196dd2f098086af032af588f5ac45a7d92
111alanyu <44562288+111alanyu@users.noreply.github.com>
2018-11-06 22:18:51 -0800

    Create GCI.py

---
+++ GCI.py
@@ -0,0 +1,10 @@
+for i in range(10):
+  print "GCI is great!"
+  i = i + 1
+print "What is your name?:" 
+name = raw_input("")
+def reverse_string1(s):
+    """Return a reversed copy of `s`"""
+    return s[::-1]
+print "Did you know your name backwards is",reverse_string1(name), "?"
+
