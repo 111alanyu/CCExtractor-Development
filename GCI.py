8138bf8b8ee5c3ed199b4ce18471fac3506e0504
111alanyu <44562288+111alanyu@users.noreply.github.com>
2018-11-06 22:19:19 -0800

    Update GCI.py

--- GCI.py
+++ GCI.py
@@ -4,7 +4,6 @@ for i in range(10):
 print "What is your name?:" 
 name = raw_input("")
 def reverse_string1(s):
-    """Return a reversed copy of `s`"""
     return s[::-1]
 print "Did you know your name backwards is",reverse_string1(name), "?"
 
